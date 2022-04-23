6a5bc61069ad97b021ab7882f4a8a2c60554daa3
Basil Eagle <basileagle@gmail.com>
2022-04-23 20:27:44 +0100

    more communication boilerplate

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btn = document.getElementById('hello');
+    const input = document.getElementById('tagInput');
     
     btn.style.backgroundColor = 'yellow';
     
     btn.addEventListener('click', () => {
+        const value = input.value;
         btn.style.backgroundColor = 'green';
-        chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+        chrome.runtime.sendMessage({tagName: value}, function(response) {
+            //console.log(response.farewell);
         });
     });
 });
